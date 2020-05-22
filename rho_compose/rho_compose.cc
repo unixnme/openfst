ff2d102e06b649a1ad29c8fdee5b0cac0d640ddd
Unix&Me <unixnme@gmail.com>
2020-05-22 14:19:16 -0700

    rho compose works

--- rho_compose/rho_compose.cc
+++ rho_compose/rho_compose.cc
@@ -6,9 +6,17 @@ int main(int argc, const char** argv) {
     auto ifst = fst::StdVectorFst::Read(argv[1]);
     auto ofst = fst::StdVectorFst::Read(argv[2]);
 
-    fst::FstPrinter<fst::StdArc> printer{*ifst,
-                                         ifst->InputSymbols(),
-                                         ifst->OutputSymbols(),
+    typedef fst::RhoMatcher<fst::SortedMatcher<fst::StdFst> > RM;
+    fst::ComposeFstOptions<fst::StdArc, RM> opts;
+    opts.gc_limit = 0;
+    opts.matcher1 = new RM{ifst, fst::MATCH_NONE, fst::kNoLabel};
+    opts.matcher2 = new RM{ofst, fst::MATCH_INPUT, 1};
+
+    fst::StdComposeFst cfst{*ifst, *ofst, opts};
+
+    fst::FstPrinter<fst::StdArc> printer{cfst,
+                                         cfst.InputSymbols(),
+                                         cfst.OutputSymbols(),
                                          nullptr,
                                          false,
                                          false,
